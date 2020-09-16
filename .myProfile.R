599f8a3f984a0c70b2e895c74c5b83343a53004c
hyginn <boris.steipe@utoronto.ca>
2020-09-16 16:32:51 +1000

    2020 updates and maintenance

---
+++ .myProfile.R
@@ -0,0 +1,23 @@
+# .myProfile.R
+# This contains information which the course framework needs from time to time
+# to personalize assignments, validate submissions etc. Make sure that
+# the information correctly matches our official records.
+# myEmail          char      A string with your eMail address. Use your official
+#                            UofT eMail address.
+# myStudentNumber  numeric   Your UofT student number. Take care to have this
+#                            correct.
+# MYSPE            char      A string with the species name of a genome-
+#                            sequenced fungus. Which species this is will be
+#                            determined in the BIN-MYSPE unit.
+#
+# NOTE:
+# After you have updated this script, save the file in your "myScripts" folder.
+# Utility scripts will look for it on the path: "./myScripts/.myProfile.R"
+#
+# ==============================================================================
+
+myEMail <- "<your-e-mail-address-here>"        # e.g. "u.franklin@utoronto.ca"
+myStudentNumber <- <your-student-number-here>  # e.g. 1003141592
+MYSPE <- "<your-adopted-species-here>"         # e.g. "Neurospora crassa OR74A"
+
+# [END]
---
+++ myScripts/README-myScripts.txt
@@ -0,0 +1,8 @@
+README - myScripts folder:
+==========================
+
+The "myScripts" folder is a place to keep your personal files
+safe. No files will be submitted into this folder on the GitHub, master
+copy. Thefore everything you put into this folder is safe from being
+inadvertently overwritten by a file with the same name that would be
+downloaded in a GitHub "pull" request.
